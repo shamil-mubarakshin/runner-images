1e171fafb70c7c98c38ed3c96203200b8aa2939e
Shamil Mubarakshin <shamil.mubarakshin@akvelon.com>
2023-06-06 14:04:36 +0200

    Troubleshoot xcode installation on Anka

--- images/macos/provision/core/xcode.ps1
+++ images/macos/provision/core/xcode.ps1
@@ -10,6 +10,9 @@ if ([string]::IsNullOrEmpty($env:XCODE_INSTALL_USER) -or [string]::IsNullOrEmpty
     throw "Required environment variables XCODE_INSTALL_USER and XCODE_INSTALL_PASSWORD are not set"
 }
 
+Write-Host "Getting Cookie"
+Get-Content -Path "~/.fastlane/spaceship/$env:XCODE_INSTALL_USER/cookie"
+
 # Spaceship Apple ID login fails due to Apple ID prompting to be upgraded to 2FA.
 # https://github.com/fastlane/fastlane/pull/18116
 $env:SPACESHIP_SKIP_2FA_UPGRADE = 1
