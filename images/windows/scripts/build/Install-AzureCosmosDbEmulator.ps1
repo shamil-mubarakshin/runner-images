3a9af1935a6f4555505dbe8ae6e4922db030cadd
Shamil Mubarakshin <shamil.mubarakshin@akvelon.com>
2024-11-19 20:19:12 +0100

    [windows] Update CosmosDB hash

--- images/windows/scripts/build/Install-AzureCosmosDbEmulator.ps1
+++ images/windows/scripts/build/Install-AzureCosmosDbEmulator.ps1
@@ -5,6 +5,6 @@
 
 Install-Binary -Type MSI `
     -Url "https://aka.ms/cosmosdb-emulator" `
-    -ExpectedSHA256Sum "9870895021BDD6512E815666279A7D57C0BE9BA5767A3340BACAF61377DC0065"
+    -ExpectedSHA256Sum "1EF64CF9CD13C611AAC394113A703AF6F525C7C39BFB6B4C3F884D57F6F1D1CF"
 
 Invoke-PesterTests -TestFile "Tools" -TestName "Azure Cosmos DB Emulator"
